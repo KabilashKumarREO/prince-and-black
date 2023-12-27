c55e0a3a7b0624fbaa8a7fb0137b85c0a8ce5dc1
Kabilash Kumar <kabilash.kumar@reodigital.com>
2023-12-27 15:16:27 +0530

    added gtm tag.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -14,14 +14,13 @@ Router.events.on("routeChangeError", () => nProgress.done());
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-T99BEBKHS4" />
-      <Script id="google-analytics">
+      <Script>
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-T99BEBKHS4');
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MSJ6WT3');
         `}
       </Script>
       <Provider store={store}>
